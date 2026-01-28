fc159cff03fa9f14c44ec1f6e61fc3649c4c6dda
harryseely <harry.seely@gmail.com>
2026-01-27 18:36:53 -0800

    update RF to use both train and val data to fit model, also modify regression plot code to manually set x and y lims

--- src/regression_utils.py
+++ src/regression_utils.py
@@ -52,31 +52,30 @@ def read_reg_obs_pred(labels_fpath, obs_pred_fpath):
     
     return df
 
-def plot_regression(obs, pred, title='', fig_out_fpath=None, hide=False):
+def plot_regression(obs, pred, ax_lims, title='', fig_out_fpath=None, hide=False):
 
     # Calculate regression metrics
     r2 = r2_score(obs, pred)
     mse = mean_squared_error(obs, pred)
     rmse = np.sqrt(mse)
     bias = np.mean(pred - obs)
+    n_samples = len(obs)
 
     # Set square figure
     fig, ax = plt.subplots(figsize=(5, 5))  # Make figure square
     ax.set(aspect='equal')
 
     # Set axis limits to be equal and tight
-    min_val = min(obs.min(), pred.min())
-    max_val = max(obs.max(), pred.max())
-    ax.set_xlim(min_val, max_val)
-    ax.set_ylim(min_val, max_val)
-
+    ax.set_xlim(ax_lims[0], ax_lims[1])
+    ax.set_ylim(ax_lims[0], ax_lims[1])
+ 
     sns.scatterplot(ax=ax, x=obs, y=pred, color='black')
-    ax.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--')
+    ax.plot([ax_lims[0], ax_lims[1]], [ax_lims[0], ax_lims[1]], color='red', linestyle='--')
     ax.set_xlabel('Observed AGB (Mg/ha)', fontsize=11)
     ax.set_ylabel('Predicted AGB (Mg/ha)', fontsize=11)
     ax.set_title(title, fontsize=14, fontweight='bold')
     ax.text(.02, .98, 
-            s = f'R²: {r2:.2f}\nRMSE: {rmse:.2f} Mg/ha\nBias: {bias:.2f} Mg/ha',
+            s = f'R²: {r2:.2f}\nRMSE: {rmse:.2f} Mg/ha\nBias: {bias:.2f} Mg/ha\nn: {n_samples}',
             ha='left', va='top', 
             transform=ax.transAxes,
             fontsize=11)
